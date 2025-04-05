35f3a83a4ef0db087b04318b3c7ccd57694decdc
YinCheng <eason010630@gmail.com>
2025-04-05 23:16:53 +0800

    Add quote with error message in not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,29 @@
 import Link from "next/link";
 
+const quote = [
+  "這個頁面就像是斷了線的風箏、脫疆的野馬、變了心的女朋友，回不來了！",
+  "紀錄組，記了工程師一個失誤...",
+  "這個頁面被恰恰打到火星去了！",
+  "問天！",
+]
+
 export default function NotFound() {
+  const randomQuote = quote[Math.floor(Math.random() * quote.length)];
   return (
-    <div className="flex flex-col items-center justify-center h-screen space-y-4">
-      <h2 className="text-6xl">404</h2>
-      <p className="text-2xl"><del>你找到了一個神秘的通道</del></p>
-      <Link className="text-xl" href="/">回首頁</Link>
+    <div className="flex flex-col gap-6 items-center justify-center h-svh select-none">
+      <h2 className="text-6xl font-mono">404</h2>
+      <p className={`
+        text-sm text-center text-gray-500
+      `}>{randomQuote}</p>
+      <Link className={`
+        px-4 py-2
+        rounded shadow-xs
+        bg-gray-300 text-gray-900
+        hover:bg-gray-400 hover:text-black hover:font-medium
+        dark:bg-gray-800 dark:text-gray-100
+        hover:dark:bg-gray-900 hover:dark:text-white
+        transition duration-200 ease-in-out
+        `} href="/">回首頁</Link>
     </div>
   );
 }
