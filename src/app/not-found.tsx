bfcfab033401e75f0b1cfb932007095a7c306062
YinCheng <eason010630@gmail.com>
2025-04-05 23:43:04 +0800

    Fix not-found page button shadow

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,25 +5,26 @@ const quote = [
   "紀錄組，記了工程師一個失誤...",
   "這個頁面被恰恰打到火星去了！",
   "問天！",
+  "火車過山洞了！"
 ]
 
 export default function NotFound() {
   const randomQuote = quote[Math.floor(Math.random() * quote.length)];
   return (
     <div className="flex flex-col gap-6 items-center justify-center h-svh select-none">
-      <h2 className="text-6xl font-mono">404</h2>
+      <h2 className="text-6xl">404</h2>
       <p className={`
         text-sm text-center text-gray-500
       `}>{randomQuote}</p>
       <Link className={`
         px-4 py-2
-        rounded shadow-xs
+        rounded shadow-md
         bg-gray-300 text-gray-900
         hover:bg-gray-400 hover:text-black hover:font-medium
         dark:bg-gray-800 dark:text-gray-100
         hover:dark:bg-gray-900 hover:dark:text-white
         transition duration-200 ease-in-out
-        `} href="/">回首頁</Link>
+        `} href="/">首頁</Link>
     </div>
   );
 }
