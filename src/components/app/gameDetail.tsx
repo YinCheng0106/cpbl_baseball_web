528648c4f42e52f982ed525963161e9fb65bea4f
YinCheng <eason010630@gmail.com>
2025-06-10 02:10:22 +0800

    feat: update player display logic in gameDetail component to use PlayerCard for better representation

--- src/components/app/gameDetail.tsx
+++ src/components/app/gameDetail.tsx
@@ -16,7 +16,7 @@ const readyPlay = (gameInfo : GameInfo) => {
             客隊先發
           </span>
           <span className="text-sm font-medium">
-            {gameInfo.awayStarter.player === null ? "未公布" : gameInfo.awayStarter.player}
+            {gameInfo.awayStarter.player === null ? "未公布" : <PlayerCard id={gameInfo.awayStarter.id} playerType="pitcher"/>}
           </span>
         </div>
         <div className="flex justify-between items-center">
@@ -24,7 +24,7 @@ const readyPlay = (gameInfo : GameInfo) => {
             主隊先發
           </span>
           <span className="text-sm font-medium">
-            {gameInfo.homeStarter.player === null ? "未公布" : gameInfo.homeStarter.player}
+            {gameInfo.homeStarter.player === null ? "未公布" : <PlayerCard id={gameInfo.homeStarter.id} playerType="pitcher"/>}
           </span>
         </div>
       </div>
@@ -48,7 +48,8 @@ const inPlaying = (
                 當前投手
               </span>
               <span className="text-sm font-medium">
-                {gameLive.onField.nowPitcher.player ? gameLive.onField.nowPitcher.player : "無"}
+                {gameLive.onField.nowPitcher.player === null ? "無" : 
+                  <PlayerCard id={gameLive.onField.nowPitcher.id} playerType="pitcher"/> }
               </span>
             </div>
             <div className="flex justify-between items-center">
@@ -56,7 +57,8 @@ const inPlaying = (
                 當前打者
               </span>
               <span className="text-sm font-medium">
-                {gameLive.onField.nowBatter.player ? gameLive.onField.nowBatter.player : "無"}
+                {gameLive.onField.nowBatter.player === null ? "無" : 
+                  <PlayerCard id={gameLive.onField.nowBatter.id} playerType="batter"/>}
               </span>
             </div>
           </div>
@@ -104,14 +106,13 @@ const gameSet = (
         <div className="space-y-2">
           <div className="flex justify-between items-center">
             <span className={`
-              text-sm border rounded p-0.5 sm:py-0.5 w-[44px] 
+              text-sm border rounded px-1 p-0.5 sm:w-[44px] 
               flex items-center justify-center 
               before:content-['勝'] sm:before:content-['勝投']`}>
             </span>
             <span className="text-sm font-medium">
-              {gameEnd.winPitcher.player === "" 
-                ? "無" : 
-                gameEnd.winPitcher.id && <PlayerCard id={gameEnd.winPitcher.id} />
+              {gameEnd.winPitcher.player === null ? "無" : 
+                <PlayerCard id={gameEnd.winPitcher.id} playerType="w/lPitcher" />
               }
             </span>
           </div>
@@ -119,13 +120,14 @@ const gameSet = (
               flex justify-between items-center
               ${gameEnd.savePitcher.id === null ? "hidden" : ""}
             `}>
-            <span className="text-sm border rounded py-0.5 w-[44px] flex items-center justify-center">
-              救援
+            <span className={`
+              text-sm border rounded px-1 p-0.5 sm:w-[44px]
+              flex items-center justify-center 
+              before:content-['救'] sm:before:content-['救援']`}>
             </span>
             <span className="text-sm font-medium">
-              {gameEnd.savePitcher.player === "" 
-                ? "無" : 
-                gameEnd.savePitcher.id && <PlayerCard id={gameEnd.savePitcher.id} />
+              {gameEnd.savePitcher.player === null ? "無" : 
+                <PlayerCard id={gameEnd.savePitcher.id} playerType="savePitcher" />
               }
             </span>
           </div>
@@ -133,14 +135,13 @@ const gameSet = (
         <div className="space-y-2">
           <div className="flex justify-between items-center">
             <span className={`
-              text-sm border rounded p-0.5 sm:py-0.5 w-[44px]
+              text-sm border rounded px-1 p-0.5 sm:w-[44px] 
               flex items-center justify-center 
               before:content-['敗'] sm:before:content-['敗投']`}>
             </span>
             <span className="text-sm font-medium">
-              {gameEnd.losePitcher.player === ""
-                ? "無" : 
-                gameEnd.losePitcher.id && <PlayerCard id={gameEnd.losePitcher.id} />
+              {gameEnd.losePitcher.player === null ? "無" : 
+                <PlayerCard id={gameEnd.losePitcher.id} playerType="w/lPitcher" />
               }
             </span>
           </div>
