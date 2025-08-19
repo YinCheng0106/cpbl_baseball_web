ede3fbe2391f01619f162a231ee71b11683c599e
YinCheng <eason010630@gmail.com>
2025-08-19 18:43:37 +0800

    feat: add icon to game and player creation headers for improved UI

--- src/app/admin/games/create/page.tsx
+++ src/app/admin/games/create/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { GameForm } from '@/components/app/gameForm';
+import { Swords } from "lucide-react";
 
 export default function CreateGame() {
 
   return (
     <div>
-      <h2 className='text-2xl font-bold mb-4'>創建比賽</h2>
+      <h2 className='flex items-center text-2xl font-bold gap-2 mb-4'><Swords /> 創建比賽</h2>
       <GameForm />
     </div>
   )
--- src/app/admin/players/create/page.tsx
+++ src/app/admin/players/create/page.tsx
@@ -2,10 +2,12 @@
 
 import { PlayerForm } from "@/components/app/playerForm";
 
+import { UserPen } from "lucide-react";
+
 export default function CreatePlayerPage() {
   return (
     <div className="">
-      <h2 className="text-2xl font-bold mb-4">創建球員</h2>
+      <h2 className="flex items-center text-2xl font-bold gap-2 mb-4"><UserPen /> 創建球員</h2>
       <PlayerForm />
     </div>
   )
